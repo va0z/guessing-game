ed33f4d5225f5c646b3e98514d528836b4ee5f78
va0z <va0z@ya.ru>
2019-10-10 23:38:21 +1000

    init: 9 fail (

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        var numMin;
+        var numMax;
+        var res;
+    }
 
     setRange(min, max) {
-
+        this.numMax = max;
+        this.numMin = min;
     }
 
     guess() {
-
+        this.res = (this.numMin + this.numMax)/2;
+        // this.res = Math.floor(this.res);
+        this.res = Math.floor(this.res);
+        // console.log (this.res);
+        // return Math.floor(this.res);
+        return this.res;
     }
 
     lower() {
-
+        this.numMax = this.res + 1;
     }
 
     greater() {
-
+        this.numMin = this.res;
     }
 }
 
