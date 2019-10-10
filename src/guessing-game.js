bea2dec8e65f2594e983b9c98ad12d25cc68a0a3
va0z <va0z@ya.ru>
2019-10-10 23:54:05 +1000

    fear: 100

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -6,25 +6,21 @@ class GuessingGame {
     }
 
     setRange(min, max) {
-        this.numMax = max;
+        this.numMax = max - 1;
         this.numMin = min;
     }
 
     guess() {
-        this.res = (this.numMin + this.numMax)/2;
-        // this.res = Math.floor(this.res);
-        this.res = Math.floor(this.res);
-        // console.log (this.res);
-        // return Math.floor(this.res);
-        return this.res;
+        this.res = Math.floor((this.numMin + this.numMax)/2);
+        return this.res + 1;
     }
 
     lower() {
-        this.numMax = this.res + 1;
+        this.numMax = this.res;
     }
 
     greater() {
-        this.numMin = this.res;
+        this.numMin = this.res + 1;
     }
 }
 
